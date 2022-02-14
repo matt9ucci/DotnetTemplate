bf3647cc053fd101e558a9a896c19119b93ff8fb
Matt Gucci <matt9ucci@gmail.com>
2022-02-14 18:33:34 +0900

    New template: Dotnet.SourceGenerator

---
+++ Dotnet.SourceGenerator/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.Text;
+global using System.Text;
---
+++ Dotnet.SourceGenerator/HelloWorldGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet.SourceGenerator;
+
+[Generator]
+public class HelloWorldGenerator : ISourceGenerator {
+	public void Execute(GeneratorExecutionContext context) {
+		StringBuilder sb = new();
+		sb.AppendLine("Console.WriteLine(\"Hello, Source Generator!\");");
+		context.AddSource("HelloWorldGenerated.g.cs", SourceText.From(sb.ToString(), Encoding.UTF8));
+	}
+
+	public void Initialize(GeneratorInitializationContext context) { }
+}
